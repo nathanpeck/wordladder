46f6330781875eb2c469ed5f340c35654ac83162
Nathan Peck <nathan.peck1@gmail.com>
2015-01-12 16:35:29 -0500

    Initial commit

---
+++ ladder-clean.js
@@ -0,0 +1,90 @@
+var dictionary = require('./dictionary.js');
+
+// Simple class for storing word ladders, with each
+// node holding a word and a reference to a previous
+// node which allows for a tree of words forming
+// ladders.
+//
+// This takes less memory than storing each potential
+// ladder as an array, as this way each word is stored
+// once at most.
+function WordLadder(word) {
+  this.prev = null;
+  this.word = word;
+}
+
+// Create a new word node referencing this word
+WordLadder.prototype.extendLadder = function(word) {
+  var result = new WordLadder(word);
+  result.prev = this;
+  return result;
+};
+
+// Traverse the word ladder starting from this leaf word
+// and return the array of words in this ladder.
+WordLadder.prototype.toArray = function() {
+  var result = [];
+  for (var curr = this; curr !== null; curr = curr.prev) {
+    result.push(curr.word);
+  }
+  return result.reverse();
+};
+
+// Given a word, find valid words that could be a bridge towards
+// the goal word.
+function findSuccessors(givenWord, dictionary) {
+  var successors = [];
+
+  // For each letter in the given word
+  for (var i = 0; i <= givenWord.word.length; ++i) {
+    // For each character in the alphabet
+    for (var ch = 'a'.charCodeAt(0); ch <= 'z'.charCodeAt(0); ++ch) {
+      // Build candidate word
+      var candidate = givenWord.word.substring(0, i) +
+                      String.fromCharCode(ch) +
+                      givenWord.word.substring(i + 1);
+      // See if the word we built is valid
+      if (dictionary[candidate] !== undefined)
+        successors.push(givenWord.extendLadder(candidate));
+    }
+  }
+
+  return successors;
+}
+
+// Given a start word, an end word, and a dictionary
+// do a breadth first search to find the shortest
+// word ladder between start word and end word.
+function findWordLadder(startWord, endWord, dictionary) {
+  var working = [
+    new WordLadder(startWord)
+  ];
+  var usedWords = {};
+
+  while (working.length !== 0) {
+    var ladder = working.shift();
+
+    // Make sure that we don't get caught in an
+    // infinite loop or repeat words in a tree.
+    if (usedWords[ladder.word] !== undefined) {
+      continue;
+    }
+    usedWords[ladder.word] = null;
+
+    // Check to see if this current word ladder has reached
+    // the destination word.
+    if (ladder.word == endWord) {
+      return ladder.toArray();
+    }
+
+    // Find potential words one letter away
+    // from the current word and add them to the work list
+    working = working.concat(findSuccessors(ladder, dictionary));
+  }
+
+  return "No word ladder found!";
+}
+
+var result = findWordLadder('chance', 'comedy', dictionary);
+
+console.log(result);
---
+++ ladder.js
@@ -0,0 +1,116 @@
+/* Instructions:
+
+   Below is a piece of code, which is functional, but extremely hard to read.
+
+   The purpose of this code is to find a word ladder between two words. The word
+   ladder is a series of words that each differ by a single letter, forming a bridge
+   between two provided words.
+
+   An example of a word ladder between "god" and "dog" might be:
+
+   god -> got -> dot -> dog
+
+   1. First pretend that the code below was sent to you for a code review
+      prior to being merged in. Write a critique of the code and what you notice
+      with it that should be improved before it can be merged in:
+
+
+
+   2. Then spend some time understanding how the code works, and refactoring it
+      to improve clarity, style and how it operates. Try to follow best practices
+      for JavaScript style, and make better use of the native JavaScript functions.
+
+   3. Afterward, please write a few thoughts about what you changed to make the code
+      better:
+
+
+
+*/
+
+function findWordLadder(startWord, endWord, dictionary) {
+  function WordLadder(word) {
+    this.prev = null;
+    this.word = word;
+    this.extendLadder = function(word) {
+      var result = new WordLadder(word);
+      result.prev = this;
+      return result;
+    };
+    this.toArray = function() {
+      var result = [];
+      for (var curr = this; curr !== null; curr = curr.prev) result.push(curr.word);
+      var reversedArray = [];
+      for (var i = result.length-1; i >= 0; i--) reversedArray[result.length-i-1] = result[i];
+      return reversedArray;
+    };
+  }
+  var working = [ new WordLadder(startWord)];
+  var usedWords = {};
+  while (working.length !== 0) {
+    var ladder = working.shift();
+    if (usedWords[ladder.word] !== undefined) continue;
+    usedWords[ladder.word] = null;
+    if (ladder.word == endWord)
+    return ladder.toArray();
+    // Find the next word
+    var successors = [];
+    for (var i = 0; i <= ladder.word.length; ++i) {
+      for (var ch = 'a'.charCodeAt(0); ch <= 'z'.charCodeAt(0); ++ch) {
+        var candidate = "";
+        for (var j = 0; j < i; j++) candidate += ladder.word[j];
+        candidate += String.fromCharCode(ch);
+        for (var k = i+1; k < ladder.word.length; k++) candidate += ladder.word[k];
+        if (dictionary[candidate] !== undefined)
+          working[working.length] = ladder.extendLadder(candidate);
+      }
+    }
+  }
+}
+
+var abbreviatedDictionary = {
+  "chance": null,
+  "change": null,
+  "changs": null,
+  "chants": null,
+  "chints": null,
+  "chines": null,
+  "chined": null,
+  "coined": null,
+  "conned": null,
+  "conner": null,
+  "conger": null,
+  "starch": null,
+  "stared": null,
+  "starer": null,
+  "stares": null,
+  "starry": null,
+  "starts": null,
+  "starve": null,
+  "stases": null,
+  "stasis": null,
+  "statal": null,
+  "stated": null,
+  "stater": null,
+  "states": null,
+  "static": null,
+  "stator": null,
+  "statue": null,
+  "status": null,
+  "staved": null,
+  "staves": null,
+  "conges": null,
+  "conies": null,
+  "conins": null,
+  "coning": null,
+  "honing": null,
+  "homing": null,
+  "hominy": null,
+  "homily": null,
+  "homely": null,
+  "comely": null,
+  "comedy": null
+};
+
+var result = findWordLadder('chance', 'comedy', abbreviatedDictionary);
+
+console.log(result);
